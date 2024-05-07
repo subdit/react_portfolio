f33dc212ba39f93fe0d54128a2bcb70ca4d0c9b6
Kasidit Bualoeng <subdit@gmail.com>
2024-05-07 16:16:54 -0400

    edited asset/about images

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 
 import styles from './About.module.css';
-// import { getImageUrl } from '../../utils';
+import { getImageUrl } from '../../utils';
 
 const About = () => {
   return (
     <section className={styles.container}>
-      <div className={styles.content}>
-        <h2 className={styles.title}>Frontend Developer</h2>
-        <p className={styles.description}>
-          I’m a front-end developer with experience in building responsive and
-          optimized sites
-        </p>
+      <h2>About</h2>
+      <div>
+        <img src={getImageUrl('about/snow_board.jpg')} alt='snow boarding' />
+        <div>
+          <ul>
+            <li>
+              <img src={getImageUrl('about/curcorIcon.png')} alt='Cursor' />
+              <div>
+                <h3>Frontend Developer</h3>
+                <p>
+                  I'm frontend developer with experience in building responsive
+                  and optimized sites
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      {/* <div className={styles.content}>
-        <h1 className={styles.title}>Backend Developer</h1>
-        <p className={styles.description}>
-          I have experience developing fast and optimised back-end systems and
-          APIs
-        </p>
-      </div>
-      <div className={styles.content}></div> */}
     </section>
   );
 };
