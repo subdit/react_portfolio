11f4938c9e68b95276fa5b1f3349de8bc8c00808
Kasidit Bualoeng <subdit@gmail.com>
2024-05-23 08:05:35 -0400

    edited image on hero and about folder

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,82 +1,49 @@
-import React from 'react';
-
 import styles from './About.module.css';
 import { getImageUrl } from '../../utils';
 
-const About = () => {
-  return (import React from "react";
-
-  import styles from "./About.module.css";
-  import { getImageUrl } from "../../utils";
-  
-  export const About = () => {
-    return (
-      <section className={styles.container} id="about">
-        <h2 className={styles.title}>About</h2>
-        <div className={styles.content}>
-          <img
-            src={getImageUrl("about/aboutImage.png")}
-            alt="Me sitting with a laptop"
-            className={styles.aboutImage}
-          />
-          <ul className={styles.aboutItems}>
-            <li className={styles.aboutItem}>
-              <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
-              <div className={styles.aboutItemText}>
-                <h3>Frontend Developer</h3>
-                <p>
-                  I'm a frontend developer with experience in building responsive
-                  and optimized sites
-                </p>
-              </div>
-            </li>
-            <li className={styles.aboutItem}>
-              <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
-              <div className={styles.aboutItemText}>
-                <h3>Backend Developer</h3>
-                <p>
-                  I have experience developing fast and optimised back-end systems
-                  and APIs
-                </p>
-              </div>
-            </li>
-            <li className={styles.aboutItem}>
-              <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
-              <div className={styles.aboutItemText}>
-                <h3>UI Designer</h3>
-                <p>
-                  I have designed multiple landing pages and have created design
-                  systems as well
-                </p>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </section>
-    );
-  };
-  
-    <section className={styles.container}>
-      <h2>About</h2>
-      <div>
-        <img src={getImageUrl('about/snow_board.jpg')} alt='snow boarding' />
-        <div>
-          <ul>
-            <li>
-              <img src={getImageUrl('about/curcorIcon.png')} alt='Cursor' />
-              <div>
-                <h3>Frontend Developer</h3>
-                <p>
-                  I'm frontend developer with experience in building responsive
-                  and optimized sites
-                </p>
-              </div>
-            </li>
-          </ul>
-        </div>
+export const About = () => {
+  return (
+    <section className={styles.container} id='about'>
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl('about/about_me.png')}
+          alt='Me sitting with a laptop'
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl('about/cursorIcon.png')} alt='Cursor icon' />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                I'm a frontend developer with experience in building responsive
+                and optimized sites
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl('about/serverIcon.png')} alt='Server icon' />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+                I have experience developing fast and optimised back-end systems
+                and APIs
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl('about/cursorIcon.png')} alt='UI icon' />
+            <div className={styles.aboutItemText}>
+              <h3>UI Designer</h3>
+              <p>
+                I have designed multiple landing pages and have created design
+                systems as well
+              </p>
+            </div>
+          </li>
+        </ul>
       </div>
     </section>
   );
 };
-
-export default About;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,17 +7,17 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Ada</h1>
+        <h1 className={styles.title}>Hi, my name is Kasidit Bualoeng</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with 5 years of experience using React and
-          NodeJS. Reach out if you'd like to learn more!
+          I'm a full-stack developer with six years of experience using React,
+          Next.js and NodeJS.
         </p>
         <a href='mailto:subdit@gmail.com' className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
       <img
-        src={getImageUrl('hero/heroImage.png')}
+        src={getImageUrl('hero/hero_profile.png')}
         alt='Hero image of me'
         className={styles.heroImg}
       />
